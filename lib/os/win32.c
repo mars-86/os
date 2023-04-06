8aa6ddca7ce1678b800b1251fc4bbb3d3bd71994
mars-86 <hrslash86@gmail.com>
2023-04-05 23:57:38 -0300

    refactor: segregate functions to create and configure a console

--- lib/os/win32.c
+++ lib/os/win32.c
@@ -32,3 +32,39 @@ HANDLE _init_instance(ContainerSize* cs)
     // TODO - return display instance
     return stdh;
 }
+
+void *get_term_stdout_handle(void)
+{
+    return GetStdHandle(STD_OUTPUT_HANDLE);
+}
+
+int get_term_buffer_info(void *handle, ContainerSize *cs)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int status = 1;
+    status = GetConsoleScreenBufferInfo(handle, &csbi);
+    cs->cs_row = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    cs->cs_col = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    return status;
+}
+
+int set_term_font(void *handle, wchar_t name, short size)
+{
+    CONSOLE_FONT_INFOEX info = { 0 };
+    info.cbSize = sizeof(info);
+    info.dwFontSize.Y = size; // leave X as zero
+    info.FontWeight = FW_NORMAL;
+    wcscpy(info.FaceName, name);
+    
+    return SetCurrentConsoleFontEx(handle, NULL, &info);
+}
+
+int set_term_cursor_info(void *handle, int visible, unsigned long size)
+{
+    CONSOLE_CURSOR_INFO lpCursor;
+    lpCursor.bVisible = visible;
+    lpCursor.dwSize = size;
+    
+    return SetConsoleCursorInfo(handle, &lpCursor);
+}
